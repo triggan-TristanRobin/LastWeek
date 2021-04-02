fa7d664aac2bf78b15850ef7a4a64e15f1f8f149
Tristan <myndmunnd@gmail.com>
2021-04-02 21:40:25 +0200

    Add default mobile projects using Blazorbindings

---
+++ Mobile/Mobile/App.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.MobileBlazorBindings;
+using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var host = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Register app-specific services
+                    //services.AddSingleton<AppState>();
+                })
+                .Build();
+
+            MainPage = new ContentPage();
+            host.AddComponent<HelloWorld>(parent: MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
