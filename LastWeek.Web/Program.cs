0f68bf43a34e32423bedcbb0c46bb4d69f13723c
Tristan Robin <tristan.robin69@gmail.com>
2022-02-06 18:26:46 +0100

    Change API project to be full web (API + Web UI) Using Blazor Using default blazor pages Bootstrap removed but references not cleaned up. Add triggan's css file

--- LastWeek.Web/Program.cs
+++ LastWeek.Web/Program.cs
@@ -1,10 +1,13 @@
 using DataManager;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IAsyncContentManager, MSSQLDBManager>();
 
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 var cnStr = builder.Configuration.GetConnectionString("LastWeek");
 builder.Services.AddDbContext<LastWeekContext>(options =>
@@ -12,7 +15,7 @@ builder.Services.AddDbContext<LastWeekContext>(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Last Week", Version = "v1" }); });
 
 var app = builder.Build();
 
@@ -20,13 +23,23 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Last Week API v1"));
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
