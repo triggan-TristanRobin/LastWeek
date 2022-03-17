835211ad8585669443ff4cd941f120c26579a58c
Tristan Robin <tristan.robin69@gmail.com>
2022-03-17 11:24:36 +0100

    Update database Add base API connection from UI

--- DataManager/Migrations/20220317101903_entity-base-type.Designer.cs
+++ DataManager/Migrations/20220317101903_entity-base-type.Designer.cs
@@ -4,6 +4,7 @@ using DataManager;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataManager.Migrations
 {
     [DbContext(typeof(LastWeekContext))]
-    partial class LastWeekContextModelSnapshot : ModelSnapshot
+    [Migration("20220317101903_entity-base-type")]
+    partial class entitybasetype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
